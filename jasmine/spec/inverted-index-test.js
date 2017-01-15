2e01c0a50d820c652bcbebd4fdc46e36d01a2c95
Jimnah Magira <jimnah.magira@andela.com>
2017-01-15 10:43:21 +0300

    Modify test spec to pass changes in inverted-index.js

--- jasmine/spec/inverted-index-test.js
+++ jasmine/spec/inverted-index-test.js
@@ -1,4 +1,17 @@
-var index = require('../../src/inverted-index.js');
+let index = require('../../src/inverted-index.js'),
+  content = `[
+            {
+              "title": "Alice in Wonderland",
+              "text": "Alice falls into a rabbit hole and enters a world full of imagination."
+            },
+
+            {
+              "title": "The Lord of the Rings: The Fellowship of the Ring.",
+              "text": "An unusual alliance of man, elf, dwarf, wizard and hobbit seek to destroy a powerful ring."
+            }
+          ]`
+          
+let filename = 'books.json';
 
 describe('Inverted Index', ()=>{
   var invertedIndex;
@@ -6,24 +19,24 @@ describe('Inverted Index', ()=>{
   describe('Read book data', ()=>{
     it('checks that json file is not empty', ()=> {
       expect(()=>{
-        index.createIndex('uploads/empty.json');
+        index.createIndex({}, filename);
       }).toThrow(new Error('the file is empty'));
     });
 
     it('checks that json file is valid', ()=> {
       expect(()=>{
-        index.createIndex('uploads/invalid.json');
+        index.createIndex('Invalid', filename);
       }).toThrow(new Error('invalid json file'));
     });
   });
 
   describe('Populate Index', () =>{
     it('checks if index is created', ()=> {
-      expect(index.createIndex('uploads/books.json')).toBeDefined();
-      expect(index.createIndex('uploads/books.json')).not.toEqual({});
+      expect(index.createIndex(content, filename)).toBeDefined();
+      expect(index.createIndex(content, filename)).not.toEqual({});
     });
     it('ensures index is correct', ()=>{
-      let indices = index.createIndex('uploads/books.json');
+      let indices = index.createIndex(content, filename);
       expect(indices['books.json']['alice']).toEqual([0]);
       expect(indices['books.json']['of']).toEqual([0,1]);
     });
@@ -31,21 +44,20 @@ describe('Inverted Index', ()=>{
 
   describe('Search Index', ()=> {
     it('checks search index returns correct result', ()=> {
-      index.createIndex('uploads/books.json');
-      index.createIndex('uploads/morebooks.json');
-      expect(index.searchIndex('books.json','alice')).toEqual({ 'books.json': { 'alice': [0]}})
+      index.createIndex(content, filename);
+      expect(index.searchIndex('books.json','alice')).toEqual({ 'books.json': { 'alice': [0]}}),
       expect(index.searchIndex('books.json', 'lord')).toEqual({ 'books.json': { 'lord': [1]}}),
-      expect(index.searchIndex('books.json', 'of')).toEqual({ 'books.json': { 'of': [0,1]}}),
-      expect(index.searchIndex('morebooks.json', 'lord')).toEqual({ 'morebooks.json': { 'lord': [1]}})
+      expect(index.searchIndex('books.json', 'of')).toEqual({ 'books.json': { 'of': [0,1]}})
+
     });
     it('ensures searchIndex can handle an array of search terms', ()=> {
-      index.createIndex('uploads/books.json');
+      index.createIndex(content, filename);
       expect( ()=>{
         index.searchIndex('books.json', ['a', 'alice'], 'book', 'me', ['help', ['me', 'out']]);
       }).not.toThrow(new Error());
     });
     it('ensures searchIndex can handle a varied number of arguments', ()=> {
-      index.createIndex('uploads/books.json');
+      index.createIndex(content, filename);
       expect(()=>{
         index.searchIndex('books.json', 'arg1');
         index.searchIndex('books.json', 'arg1', 'arg2');
