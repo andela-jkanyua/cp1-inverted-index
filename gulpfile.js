b2af032e47157a2a1c530f0b5407ab8d854e01a7
Jimnah Magira <jimnah.magira@andela.com>
2017-01-20 12:18:16 +0300

    lint gulp file and change syntax to es6

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,31 @@
 /* File: gulpfile.js */
 
 // grab our packages
-var gulp 			= require('gulp'),
-    jshint 			= require('gulp-jshint'),
-    //jasmineBrowser 	= require('gulp-jasmine-browser'),
-    jasmineNode = require('gulp-jasmine-node');
-    watch = require('gulp-watch');
+const gulp = require('gulp');
+const jshint = require('gulp-jshint');
+const jasmineNode = require('gulp-jasmine-node');
 
 // define the default task and add the watch task to it
 gulp.task('default', ['watch']);
 
 // configure the jshint task
-gulp.task('lint', function() {
+gulp.task('lint', () => {
   return gulp.src(['./src/**/*.js', './jasmine/spec/**/*.js'])
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'));
+  .pipe(jshint())
+  .pipe(jshint.reporter('jshint-stylish'));
 });
 
 // configure which files to watch and what tasks to use on file changes
-gulp.task('watch', function() {
-  gulp.watch(['./src/**/*.js', './jasmine/spec/**/*.js' ], ['lint']);
+gulp.task('watch', () => {
+  gulp.watch(['./src/**/*.js', './jasmine/spec/**/*.js'], ['lint']);
 });
 
-
-gulp.task('test', function () {
-    return gulp.src(['./src/**/*.js', './jasmine/spec/**/*.js'])
-    .pipe(jasmineNode({
-        timeout: 10000,
-        includeStackTrace: false,
-        color: true
-    }));
+// configure gul to run jasmine-node tests
+gulp.task('test', () => {
+  return gulp.src(['./src/**/*.js', './jasmine/spec/**/*.js'])
+  .pipe(jasmineNode({
+    timeout: 10000,
+    includeStackTrace: false,
+    color: true,
+  }));
 });
-
