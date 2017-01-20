8ac9a3d00caf6a4ce17b16c5cf444a2e9ad30951
Jimnah Magira <jimnah.magira@andela.com>
2017-01-20 12:24:55 +0300

    Add error and success properties to scope to log errors and success on frontend

--- public/core.js
+++ public/core.js
@@ -1,42 +1,52 @@
 // public/core.js
 function mainController($scope, $http) {
-    $scope.formData = {};
-    $http.get('/api/createindex')   
-        .success(function(data) {
-            $scope.invertedIndex = data;
-            $scope.filelength = Object.keys(data).length;
-            console.log(data);
-        })
-        .error(function(data) {
-            console.log('Error: ' + data);
-        });
+  $scope.formData = {};
+  $http.get('/api/createindex')
+  .success((data) => {
+    $scope.error = false;
+    $scope.success = false;
+    $scope.invertedIndex = data;
+    $scope.filelength = Object.keys(data).length;
+  })
+  .error((data) => {
+    $scope.success = false;
+    $scope.error = data.message;
+  });
     // when submitting the add form, send the fileDetails{name} to the node API
-    $scope.createIndex = function() {
+  $scope.createIndex = function () {
+    if ($scope.formData.fileDetails.data === 'data:') {
+      $scope.success = false;
+      $scope.error = 'The File is empty';
+      throw ('Empty File');
+    }
 
-        $http.post('/api/createindex', $scope.formData.fileDetails)
-            .success(function(data) {
-                $scope.formData = {}; // clear the form so our user is ready to enter another
-                $scope.invertedIndex = data;
-                $scope.filelength = Object.keys(data).length;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log($scope);
-                console.log('Error: ' + data);
-            });
-    };
-    //Submit search terms and send FormData {file, SeachTerms} to node API
-    $scope.searchIndex = function() {
-        $http.post('/api/search', $scope.formData)
-            .success(function(data) {
-                $scope.formData = {}; // clear the form so our user is ready to enter another
-                $scope.searchIndexes = data;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
-    };
+    $http.post('/api/createindex', $scope.formData.fileDetails)
+    .success((data) => {
+      $scope.formData = {}; // clear the form so our user is ready to enter another
+      $scope.invertedIndex = data;
+      $scope.filelength = Object.keys(data).length;
+      $scope.error = false;
+      $scope.success = 'Success: Index created successfully!';
+    })
+    .error((data) => {
+      $scope.success = false;
+      $scope.error = data.message;
+      
+    });
+  };
+  // submit search terms and send FormData {file, SeachTerms} to node API
+  $scope.searchIndex = function () {
+    $http.post('/api/search', $scope.formData)
+    .success((data) => {
+      $scope.formData = {}; // clear the form so our user is ready to enter another
+      $scope.searchIndexes = data;
+      console.log($scope.searchIndexes);
+    })
+    .error((data) => {
+      $scope.success = false;
+      console.log(`Error: ${data}`);
+    });
+  };
 }
 mainController.$inject = ['$scope', '$http'];
-angular.module('cpInvertedIndex', ["ng-file-model"]).controller('mainController', mainController);
+angular.module('cpInvertedIndex', ['ng-file-model']).controller('mainController', mainController);
